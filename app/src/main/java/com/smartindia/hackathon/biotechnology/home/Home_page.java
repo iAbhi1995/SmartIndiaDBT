e04a24911baeae613ed07fab0f534fbcd2793795
Ayush <ayushjain17aug@gmail.com>
2017-03-27 17:57:17 +0530

    internship.xml

--- app/src/main/java/com/smartindia/hackathon/biotechnology/home/Home_page.java
+++ app/src/main/java/com/smartindia/hackathon/biotechnology/home/Home_page.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.smartindia.hackathon.biotechnology.R;
+
 public class Home_page extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
